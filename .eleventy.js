58b78fd05afee65cf46defb708a876a245a7fd30
Keir Davis <minecraftfun201@gmail.com>
2022-03-11 20:53:33 +0000

    Made articles

---
+++ .eleventy.js
@@ -0,0 +1,19 @@
+const {DateTime} = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy("src/assets/");
+
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+    });
+
+
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        },
+    }
+
+}
